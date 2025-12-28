5a5b11a401e32a4bf24d9c39dde7fb55bd92f8fa
eliza <melizahiyori@yahoo.com>
2025-12-28 21:37:52 +0900

    Add files via upload

---
+++ week1/mario-less/mario.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while(height < 1 || height > 8);
+
+    int i, j, k;
+    for(i = 0; i< height; i++)
+    {
+        for( j= (height-1); j> i; j--)
+        {
+            printf(" ");
+        }
+        for( k=0; k<= i; k++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
+
+
